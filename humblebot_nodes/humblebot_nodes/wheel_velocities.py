10046022cca7a83b84aa902d8386a5260f18d144
Kikiman2 <gati.krisztian10@gmail.com>
2023-11-17 14:21:49 +0100

    Improved the logic if the wheel velocity measuments

--- humblebot_nodes/humblebot_nodes/wheel_velocities.py
+++ humblebot_nodes/humblebot_nodes/wheel_velocities.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-
-from std_msgs.msg import Float32MultiArray
-from example_interfaces.msg import Int16
+from geometry_msgs.msg import Twist
 
 import math
+
 
 
 class RobotWheelVelocityControlServerNode(Node):
@@ -13,59 +12,87 @@ class RobotWheelVelocityControlServerNode(Node):
         super().__init__("robot_wheel_control")
 
         self.subscriber_ = self.create_subscription(
-            Int16, "robot_direction", self.callback_robot_direction, 10)
+            Twist, "cmd_vel", self.callback_robot_direction, 10)
 
-        self.publisher = self.create_publisher(Float32MultiArray, 'float32_multiarray_topic', 10)
-        self.timer = self.create_timer(1.0, self.publish_array)
+        self.publisher = self.create_publisher(Twist, '/humble/cmd_vel', 10)
+        self.timer = self.create_timer(1.0, self.publish_data)
         
-        self.v1 = 0
-        self.v2 = 0
-        self.v3 = 0
+        self.wheel1 = 0
+        self.wheel2 = 0
+        self.wheel3 = 0
 
-        self.Speed = 1.0  # Robot speed
-        # self.theta = math.pi / 4  # Orientation angle (45 degrees)
-        self.d = 0.1  # Distance from the center to the wheel contact point
-        self.robot_orientation = 0
+        self.angular_velocity_ = self.rpm_to_angular_velocity(400)  # Robot angular velocity in rad/s
+        self.Speed = 1.0                                            # Robot speed in m/s
+        self.robot_orientation = 0                                  # Calculated in radians
+        self.d_ = 0.1                                               # Distance from the center to the wheel contact point in meters
+        
 
-    def publish_array(self):
-        msg = Float32MultiArray()
-        msg.data = [1.0, 2.0, 3.0]  # Modify this list with your desired data
+    def publish_data(self):
+        msg = Twist()
+        msg.linear.x = self.wheel1
+        msg.linear.y = self.wheel2
+        msg.linear.z = self.wheel3
         self.publisher.publish(msg)
 
     def callback_robot_direction(self, msg):
         robot_direction = msg.data
         velocities = []
-        velocities = self.calculate_wheel_velocities(
-            self.Speed, robot_direction)
-        
+
+        # TODO calculate the robot direction from /cmd_vel linear x and y
+        velocities = self.calculate_wheel_velocities(self.Speed, robot_direction, self.angular_velocity_, self.d_)
+        robot_direction
         self.get_logger().info("---")
         self.get_logger().info(str(robot_direction))
         self.get_logger().info(str(velocities[0]))
-        self.v1 = velocities[0]
+        self.wheel1 = velocities[0]
         self.get_logger().info(str(velocities[1]))
         self.v2 = velocities[1]
         self.get_logger().info(str(velocities[2]))
         self.v3 = velocities[2]
 
-    def calculate_wheel_velocities(self, robot_speed, robot_angle):
+    def calculate_wheel_velocities(self, robot_speed, robot_orientation, angular_velocity, d):
+        """
+        Calculate the velocities of each wheel in a three-wheeled omnidirectional robot.
 
-        wheel_angles = (60, 180, 300)
+        :param robot_speed: The linear speed of the robot.
+        :param robot_orientation: The orientation angle of the robot (direction of movement).
+        :param angular_velocity: The rotational velocity of the robot.
+        :param d: # Distance from the center to each wheel.
+        :return: A tuple containing the velocities of the three wheels.
+        """
+        # Angles of wheels in degrees (0, 120, 240 degrees)
+        wheel_angles = [0, 120, 240]
 
+        # Convert angles to radians
+        robot_orientation_rad = math.radians(robot_orientation)
+        wheel_angles_rad = [math.radians(angle) for angle in wheel_angles]
+
+        # Calculate wheel velocities
         wheel_velocities = []
 
-        for angle in wheel_angles:
+        for angle in wheel_angles_rad:
+            # Velocity contribution due to linear speed
+            linear_component = robot_speed * math.cos(robot_orientation_rad - angle)
 
-            linear_velocity = robot_speed * \
-                math.sin(math.radians(robot_angle-angle))
+            # Velocity contribution due to rotation
+            rotational_component = angular_velocity * d
 
-            # Calculate the wheel velocity
-            wheel_velocity = round(linear_velocity, 3)
-
-            wheel_velocities.append(float(wheel_velocity))
+            # Total wheel velocity
+            wheel_velocity = linear_component - rotational_component
+            wheel_velocities.append(wheel_velocity)
 
         return wheel_velocities
+    
+    def rpm_to_angular_velocity(self, rpm):
+        """
+        Convert RPM (Revolutions Per Minute) to Angular Velocity (Radians per Second).
 
+        :param rpm: Revolutions Per Minute
+        :return: Angular velocity in Radians per Second
+        """
+        return rpm * (2 * math.pi / 60)
 
+    
 def main(args=None):
     rclpy.init(args=args)
     node = RobotWheelVelocityControlServerNode()
